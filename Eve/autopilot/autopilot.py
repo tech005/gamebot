df5e900a96552536539dac1e88ea359e788f5562
ryan <ryansgrogers@outlook.com>
2020-08-19 16:55:54 -0300

    functional programming

--- Eve/autopilot/autopilot.py
+++ Eve/autopilot/autopilot.py
@@ -1,20 +1,30 @@
 import pyautogui
 import time
+import os
 
-while True:
-    locatestation = pyautogui.locateOnScreen('C:/Users/ryans/OneDrive/PROGRAMMING/gamebot/Eve/autopilot/station.PNG', confidence=0.8)
-    locategate = pyautogui.locateOnScreen('C:/Users/ryans/OneDrive/PROGRAMMING/gamebot/Eve/autopilot/gate.PNG', confidence=0.8)
-    if (locategate != None):
-        print("gate found")
-        pyautogui.keyDown('d')
-        pyautogui.click(locategate)
-        pyautogui.keyUp('d')
-    elif (locatestation != None):
-        print("station found")
-        pyautogui.keyDown('d')
-        pyautogui.click(locategate)
-        pyautogui.keyUp('d')
-    else:
-        print("no destination found")
-        break
-    time.sleep(1)
+workingpath = os.getcwd()
+stationpng = workingpath+'\Eve\\autopilot\station.PNG'
+gatepng = workingpath+'\Eve\\autopilot\gate.PNG'
+
+def autopilot(stationpng,gatepng):
+    while True:
+        locatestation = pyautogui.locateOnScreen(stationpng, confidence=0.8)
+        locategate = pyautogui.locateOnScreen(gatepng, confidence=0.8)
+        if (locategate != None):
+            print("gate found")
+            pyautogui.keyDown('d')
+            pyautogui.click(locategate)
+            pyautogui.keyUp('d')
+            time.sleep(1)
+        elif (locatestation != None):
+            print("station found")
+            pyautogui.keyDown('d')
+            pyautogui.click(locatestation)
+            pyautogui.keyUp('d')
+            time.sleep(1)
+        else:
+            print("no destination found have you arrived?")
+            time.sleep(5)
+
+
+autopilot(stationpng,gatepng)
